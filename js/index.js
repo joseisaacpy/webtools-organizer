009182635427ddc4104fd6555a4ebee2ee5d2632
José Isaac <joseisaacnascimento@gmail.com>
2025-04-27 13:19:56 -0300

    feat: Criação dos containers de links e da lógica de abrir modal do formulário

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// section de adicionar links
+const sectionForm = document.getElementById("form-section");
+// botão de adicionar links novos
+const btnAddLink = document.getElementById("btn-add-link");
+
+// botão recebe evento de clique, onde vai mostrar o form para adicionar links
+btnAddLink.addEventListener("click", function () {
+  // se o form estiver visivel, ele esconde, se não, ele mostra
+  if (sectionForm.style.display == "flex") {
+    sectionForm.style.display = "none";
+  } else {
+    sectionForm.style.display = "flex";
+  }
+});
